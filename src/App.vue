df93c0d05763231f2fa0d298296e31f9fe89dc90
Srinath Ravichandran <srinath.r1995@gmail.com>
2020-06-18 23:16:59 +0530

    ep7 added toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app>
-    <v-content>
+    <Navbar></Navbar>
+    <v-content class="grey lighten-4">
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
   name: "App",
+  components: { Navbar },
   data: () => ({
     //
   })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <!-- https://vuetifyjs.com/en/styles/display/#visibility -->
-    <v-btn class="hidden-md-and-down">click Me Down</v-btn>
-    <v-btn class="hidden-md-and-up">click Me Up</v-btn>
-    <v-btn class="hidden-sm-only">click Me Only</v-btn>
   </div>
 </template>
 
