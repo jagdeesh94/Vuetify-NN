df63a3715c76983b40d94d84be63e48847d7f651
Srinath Ravichandran <srinath.r1995@gmail.com>
2020-06-18 23:45:35 +0530

    ep8 nav drawer added with app-bar-nav-icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-app-bar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
@@ -11,9 +12,20 @@
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-app-bar>
+    <!-- app is needed here , so that nav-drawer will come on top of the toolbar also.If not given , 
+    then toolbar will be on top of nav drawer and a part of nav drawer will be overlapped by toolbar-->
+    <v-navigation-drawer app class="indigo" v-model="drawer">
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
 </script>
