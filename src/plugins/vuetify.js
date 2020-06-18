d96ff3a51b26074451a11cc817ae515fcd29cd03
Srinath Ravichandran <srinath.r1995@gmail.com>
2020-06-18 21:06:34 +0530

    ep5 done (but there is no space between buttons..check later)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+library.add(fas) // Include needed icons
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    iconfont: 'fa'
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <p
-      class="red white--text"
-    >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae dolorum atque explicabo repellendus harum debitis commodi, animi quo doloribus vitae vero ea dicta consequuntur, saepe libero perspiciatis inventore distinctio reprehenderit.</p>
-    <p
-      class="pink lighten-4 red--text text--lighten-4"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quam veritatis minima nostrum at maiores rem, iusto voluptatum, tempore sunt neque temporibus quibusdam asperiores, impedit alias aperiam? Ab, neque porro.</p>
-    <p class="display-4">Massive Display</p>
-    <p class="display-1">Small Display</p>
-    <p class="title font-weight-bold">Text Title</p>
-    <p class="subheading font-weight-thin">Text Sub HEading</p>
-    <!--Displays range form 1 to 4-->
-    <!--check https://v15.vuetifyjs.com/en/framework/typography-->
+    <v-btn color="pink white--text">Click Me</v-btn>
+    <!--To remove the shadow from the button, use depressed prop-->
+    <v-btn depressed color="yellow purple--text">Press</v-btn>
+    <!--To show the bg of button as transparent, use text prop-->
+    <v-btn text color="yellow pink--text">Click Me</v-btn>
+    <!--To show the bg of button as rounded, use rounded prop-->
+    <v-btn rounded color="pink yellow--text">Click Me</v-btn>
+
+    <!--refer this for buttons https://vuetifyjs.com/en/components/buttons/#buttons-->
+
+    <!--https://cdn.materialdesignicons.com/1.1.34/-->
+    <v-btn rounded color="yellow purple--text">
+      <v-icon>mdi-mail</v-icon>
+    </v-btn>
+    <!--https://fontawesome.com/icons/accessible-icon?style=brands-->
+    <v-btn rounded color="yellow purple--text">
+      <v-icon>fa-address-book</v-icon>
+    </v-btn>
+    <!--check https://v15.vuetifyjs.com/en/f<ramework/typography-->
+
+    <v-btn depressed class="pink white--text">
+      <v-icon>mdi-mail</v-icon>
+      <span>Email Me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-mail</v-icon>
+      <span>Email Me</span>
+    </v-btn>
+
+    <v-btn depressed large color="pink white--text">
+      <span>Email Me</span>
+      <v-icon right large>mdi-mail</v-icon>
+    </v-btn>
+
+    <v-btn depressed small dark color="pink white--text">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="pink white--text">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
   </div>
 </template>
 
