65107385da22ccc64398ecc3d0ba20bf3c9f7b63
Srinath Ravichandran <srinath.r1995@gmail.com>
2020-06-19 01:18:13 +0530

    ep 10; added list to nab bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,19 @@
     <!-- app is needed here , so that nav-drawer will come on top of the toolbar also.If not given , 
     then toolbar will be on top of nav drawer and a part of nav drawer will be overlapped by toolbar-->
     <v-navigation-drawer app class="primary" v-model="drawer">
-      <p>test</p>
+      <!-- Something like ul in html -->
+      <v-list>
+        <!-- Something like li in html -->
+        <!-- router prop is not mandatory, v-bind:to routes to that component: which u can see in the address -->
+        <v-list-item v-for="link in links" :key="link.text" router v-bind:to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -24,7 +36,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" }
+      ]
     };
   }
 };
