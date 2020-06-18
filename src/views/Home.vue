4ea5f7935b8ba997fef6296ce127db8457901bf8
Srinath Ravichandran <srinath.r1995@gmail.com>
2020-06-18 22:38:29 +0530

    visibility based on screen size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,10 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <v-btn color="pink white--text">Click Me</v-btn>
-    <!--To remove the shadow from the button, use depressed prop-->
-    <v-btn depressed color="yellow purple--text">Press</v-btn>
-    <!--To show the bg of button as transparent, use text prop-->
-    <v-btn text color="yellow pink--text">Click Me</v-btn>
-    <!--To show the bg of button as rounded, use rounded prop-->
-    <v-btn rounded color="pink yellow--text">Click Me</v-btn>
-
-    <!--refer this for buttons https://vuetifyjs.com/en/components/buttons/#buttons-->
-
-    <!--https://cdn.materialdesignicons.com/1.1.34/-->
-    <v-btn rounded color="yellow purple--text">
-      <v-icon>mdi-mail</v-icon>
-    </v-btn>
-    <!--https://fontawesome.com/icons/accessible-icon?style=brands-->
-    <v-btn rounded color="yellow purple--text">
-      <v-icon>fa-address-book</v-icon>
-    </v-btn>
-    <!--check https://v15.vuetifyjs.com/en/f<ramework/typography-->
-
-    <v-btn depressed class="pink white--text">
-      <v-icon>mdi-mail</v-icon>
-      <span>Email Me</span>
-    </v-btn>
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>mdi-mail</v-icon>
-      <span>Email Me</span>
-    </v-btn>
-
-    <v-btn depressed large color="pink white--text">
-      <span>Email Me</span>
-      <v-icon right large>mdi-mail</v-icon>
-    </v-btn>
-
-    <v-btn depressed small dark color="pink white--text">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="pink white--text">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <!-- https://vuetifyjs.com/en/styles/display/#visibility -->
+    <v-btn class="hidden-md-and-down">click Me Down</v-btn>
+    <v-btn class="hidden-md-and-up">click Me Up</v-btn>
+    <v-btn class="hidden-sm-only">click Me Only</v-btn>
   </div>
 </template>
 
