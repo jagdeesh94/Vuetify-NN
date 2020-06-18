0d29b13743d02b7ee38c7798eb300a631021ada0
Srinath Ravichandran <srinath.r1995@gmail.com>
2020-06-18 17:32:59 +0530

    ep4 completed:Text&colors...refer the attched documentation commented

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae dolorum atque explicabo repellendus harum debitis commodi, animi quo doloribus vitae vero ea dicta consequuntur, saepe libero perspiciatis inventore distinctio reprehenderit.</p>
+    <p
+      class="pink lighten-4 red--text text--lighten-4"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quam veritatis minima nostrum at maiores rem, iusto voluptatum, tempore sunt neque temporibus quibusdam asperiores, impedit alias aperiam? Ab, neque porro.</p>
+    <p class="display-4">Massive Display</p>
+    <p class="display-1">Small Display</p>
+    <p class="title font-weight-bold">Text Title</p>
+    <p class="subheading font-weight-thin">Text Sub HEading</p>
+    <!--Displays range form 1 to 4-->
+    <!--check https://v15.vuetifyjs.com/en/framework/typography-->
   </div>
 </template>
 
